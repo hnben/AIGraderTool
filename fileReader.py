f66b87862a524cb7e9f3b4cd5dd94b19ba3e23ed
hnben <hnguyen10082000@gmail.com>
2025-01-30 07:18:37 -0800

    implemented csv seperation from sensitive data and Groq

--- fileReader.py
+++ fileReader.py
@@ -1,24 +1,35 @@
+import os
+import re
 import pandas as pd
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('CAPSTONE TEST.csv')
+from groq import Groq
+from dotenv import load_dotenv
 
-#check column names
-#print(df.columns)
-# question1 = df[['178498165: No more than 2-3 sentences\nList 2 potential challenges with implementing an RL agent for the game you chose in question #1. Briefly explain the problem / concern that each present.']]
-# print("QUESTION 1: "+question1)
-# Access specific columns
-print(df[['name', 'id', 'score', 'n correct' , 'n incorrect']])
+#seperate the questions into a new csv 
+df = pd.read_csv('data.csv')
+pattern = r'^\d+:.*'
 
-#print(df.info())
-# question1 = df.iloc[:, 9]
-# question2 = df.iloc[:,11]
-# question3 = df.iloc[:, 13]
-# print(question1)
-# print(question2)
-# print(question3)
+# Find columns that match the pattern
+matching_columns = [col for col in df.columns if re.match(pattern, col)]
 
+new_df = df[matching_columns]
+new_df.to_csv('questions.csv', index=False)
 
-# calculate the average score
-average_score = df['score'].mean()
-print(f'Average score: {average_score}')
+#run the questions through Groq
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
